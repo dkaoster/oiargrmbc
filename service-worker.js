4f3db2fbbe9662a7a1b58f9b32579b54862554ce
dkaoster <dkao@diplateevo.com>
2021-12-11 04:24:55 +0000

    Deploying to gh-pages from  @ 310c9b2b0ac7767311f0724d19c3cc8971ec4c06 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,83 @@
-!function(){"use strict";const a=1630401453887,t="cache"+a,e=["/client/client.7583b04e.js","/client/inject_styles.5607aec6.js","/client/index.9dd00721.js","/client/Card.ec8690f0.js","/client/letters.79743f82.js"].concat(["/service-worker-index.html","/android-chrome-192x192.png","/android-chrome-512x512.png","/apple-touch-icon.png","/data/all_chars.json","/data/mandarin-1000.csv","/data/mandarin-10000.csv","/data/mandarin-2000.csv","/data/mandarin-3000.csv","/data/mandarin-4000.csv","/data/mandarin-5000.csv","/data/mandarin-6000.csv","/data/mandarin-7000.csv","/data/mandarin-8000.csv","/data/mandarin-9000.csv","/favicon-16x16.png","/favicon-32x32.png","/favicon.ico","/global.css","/oiargrmbc.jpg","/site.webmanifest"]),n=new Set(e);self.addEventListener("install",(a=>{a.waitUntil(caches.open(t).then((a=>a.addAll(e))).then((()=>{self.skipWaiting()})))})),self.addEventListener("activate",(a=>{a.waitUntil(caches.keys().then((async a=>{for(const e of a)e!==t&&await caches.delete(e);self.clients.claim()})))})),self.addEventListener("fetch",(t=>{if("GET"!==t.request.method||t.request.headers.has("range"))return;const e=new URL(t.request.url),c=e.protocol.startsWith("http"),s=e.hostname===self.location.hostname&&e.port!==self.location.port,i=e.host===self.location.host&&n.has(e.pathname),r="only-if-cached"===t.request.cache&&!i;!c||s||r||t.respondWith((async()=>i&&await caches.match(t.request)||async function(t){const e=await caches.open("offline"+a);try{const a=await fetch(t);return e.put(t,a.clone()),a}catch(a){const n=await e.match(t);if(n)return n;throw a}}(t.request))())}))}();
+const timestamp = 1639196693438;
+const build = [
+  "/_app/start-940530d7.js",
+  "/_app/assets/start-61d1577b.css",
+  "/_app/pages/__layout.svelte-c6c5d530.js",
+  "/_app/assets/pages/__layout.svelte-fe06854d.css",
+  "/_app/pages/__error.svelte-9c4c3056.js",
+  "/_app/assets/pages/__error.svelte-217c36c1.css",
+  "/_app/pages/index.svelte-b45ff714.js",
+  "/_app/assets/pages/index.svelte-de4cb07a.css",
+  "/_app/pages/letters.svelte-000dfe81.js",
+  "/_app/assets/pages/letters.svelte-1b0fd4f2.css",
+  "/_app/chunks/vendor-8127e5b2.js",
+  "/_app/chunks/Card-88880209.js",
+  "/_app/assets/Card-30ff22cd.css"
+];
+const files = [
+  "/android-chrome-192x192.png",
+  "/android-chrome-512x512.png",
+  "/apple-touch-icon.png",
+  "/data/all_chars.json",
+  "/data/mandarin-1000.csv",
+  "/data/mandarin-10000.csv",
+  "/data/mandarin-2000.csv",
+  "/data/mandarin-3000.csv",
+  "/data/mandarin-4000.csv",
+  "/data/mandarin-5000.csv",
+  "/data/mandarin-6000.csv",
+  "/data/mandarin-7000.csv",
+  "/data/mandarin-8000.csv",
+  "/data/mandarin-9000.csv",
+  "/favicon-16x16.png",
+  "/favicon-32x32.png",
+  "/favicon.ico",
+  "/global.css",
+  "/oiargrmbc.jpg",
+  "/site.webmanifest"
+];
+const ASSETS = `cache${timestamp}`;
+const to_cache = build.concat(files);
+const staticAssets = new Set(to_cache);
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(ASSETS).then((cache) => cache.addAll(to_cache)).then(() => {
+    self.skipWaiting();
+  }));
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then(async (keys) => {
+    for (const key of keys) {
+      if (key !== ASSETS)
+        await caches.delete(key);
+    }
+    self.clients.claim();
+  }));
+});
+async function fetchAndCache(request) {
+  const cache = await caches.open(`offline${timestamp}`);
+  try {
+    const response = await fetch(request);
+    cache.put(request, response.clone());
+    return response;
+  } catch (err) {
+    const response = await cache.match(request);
+    if (response)
+      return response;
+    throw err;
+  }
+}
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || event.request.headers.has("range"))
+    return;
+  const url = new URL(event.request.url);
+  const isHttp = url.protocol.startsWith("http");
+  const isDevServerRequest = url.hostname === self.location.hostname && url.port !== self.location.port;
+  const isStaticAsset = url.host === self.location.host && staticAssets.has(url.pathname);
+  const skipBecauseUncached = event.request.cache === "only-if-cached" && !isStaticAsset;
+  if (isHttp && !isDevServerRequest && !skipBecauseUncached) {
+    event.respondWith((async () => {
+      const cachedAsset = isStaticAsset && await caches.match(event.request);
+      return cachedAsset || fetchAndCache(event.request);
+    })());
+  }
+});
